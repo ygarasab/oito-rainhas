12239331025c0267f6e025e7c624c85f3a500e2c
Gabriela Maximino <39780413+gabrielamaximino@users.noreply.github.com>
2020-11-19 23:19:40 -0300

    ops rsrs
    tinha feito errado agora sim ta dando resultado certo e melhor

--- algoritmos/shc/caixinha.py
+++ algoritmos/shc/caixinha.py
@@ -5,7 +5,7 @@ def reduz_temperatura(temperatura):
     return temperatura - 0.01
 
 def tabuleiro_deve_mudar(temperatura, variacao):
-    exp = decimal.Decimal(decimal.Decimal(math.e) ** (decimal.Decimal(-variacao) * decimal.Decimal(temperatura)))
+    exp = decimal.Decimal(decimal.Decimal(math.e) ** (decimal.Decimal(variacao) / decimal.Decimal(temperatura)))
 
     if random.uniform(0, 1) < exp:
         return True
