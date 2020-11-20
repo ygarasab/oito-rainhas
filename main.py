7806fc28822d1c99863ef6d584ec46bfc1393005
Igor Matheus Souza Moreira <imatheus.sm@gmail.com>
2020-11-20 10:05:35 -0300

    Remoção de comandos agora desnecessários
    Para além do exposto no título deste commit, outros pequenos detalhes foram alterados para eliminar avisos do PyCharm.

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from tabuleiro import Tabuleiro
 
 tab1 = Tabuleiro(4)
 tab2 = Tabuleiro(4)
-tab1.rainhas = [2, 1, 2, 1] # exemplos do slide 12 só pra testar se tá calculando certo o valor
+tab1.rainhas = [2, 1, 2, 1]  # exemplos do slide 12 pra testar se tá calculando o valor certo
 tab2.rainhas = [2, 0, 2, 1]
-tab1.calcula_valor()
-tab2.calcula_valor()
-print(f"Valor do tab1: {tab1.valor}  Valor do tab2: {tab2.valor}")
+
+print(f"Valor do tab1: {tab1.valor}\n"
+      f"Valor do tab2: {tab2.valor}")
 
 shc.simulated_annealing(100)
