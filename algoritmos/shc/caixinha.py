c1f39d3b894ea6a806a97327155c64a69cba9a73
Igor Matheus Souza Moreira <imatheus.sm@gmail.com>
2020-11-20 10:05:07 -0300

    feat : uso do numpy no lugar do math e import statements otimizados
    Para além do citado no título do commit e como nos commits anteriores, a verificação de ortografia do PyCharm foi suprimida para as duas funções contidas no arquivo até então.
    
    Um comentário: será mesmo que o decaimento da temperatura é linear, conforme está agora?

--- algoritmos/shc/caixinha.py
+++ algoritmos/shc/caixinha.py
@@ -1,10 +1,14 @@
-import random, math, decimal
+import decimal
+import numpy as np
 
 
+# noinspection SpellCheckingInspection
 def reduz_temperatura(temperatura):
     return temperatura - 0.01
 
 
+# noinspection SpellCheckingInspection
 def tabuleiro_deve_mudar(temperatura, variacao):
-    exp = decimal.Decimal(decimal.Decimal(math.e) ** (decimal.Decimal(variacao) / decimal.Decimal(temperatura)))
-    return random.uniform(0, 1) < exp
+    exp = decimal.Decimal(decimal.Decimal(np.e) ** (decimal.Decimal(variacao) / decimal.Decimal(temperatura)))
+
+    return np.random.uniform() < exp
