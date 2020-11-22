8a5f3b1f9e21bd16306733148eff0c70dd4bddb0
Igor Matheus Souza Moreira <imatheus.sm@gmail.com>
2020-11-22 16:07:07 -0300

    Recozimento simulado aprimorado para obtenção vetorizada de temperaturas
    O algoritmo foi aprimorado de forma a obter de antemão todas as temperaturas a serem utilizadas nos loops ao invés de obtê-las uma-a-uma.

--- algoritmos/shc/shc.py
+++ algoritmos/shc/shc.py
@@ -1,26 +1,18 @@
-from tabuleiro import Tabuleiro
+from algoritmos.tabuleiro import Tabuleiro
 from .caixinha import *
 
 
 # noinspection SpellCheckingInspection
-def simulated_annealing(temperatura_inicial, verboso=False):
+def simulated_annealing(temperatura_inicial, variacao, verboso=False):
     tabuleiro_atual = Tabuleiro()
-    temperatura, iteracao = temperatura_inicial, 0
+
+    n_iteracoes = np.round(np.floor(temperatura_inicial / variacao))
+    temperaturas = temperatura_inicial - variacao * np.arange(n_iteracoes)
 
     if verboso is True:
-        print(f"[Iteração 0] {tabuleiro_atual}")
+        print(f"[Iteração -1] {tabuleiro_atual}")
 
-    while True:
-        iteracao += 1
-
-        temperatura = reduz_temperatura(temperatura)
-
-        if temperatura < 0:
-            if verboso is True:
-                print("A temperatura chegou a zero.")
-
-            return tabuleiro_atual
-
+    for iteracao, temperatura in enumerate(temperaturas):
         tabuleiro_seguinte = Tabuleiro()
 
         variacao = tabuleiro_atual - tabuleiro_seguinte
@@ -31,4 +23,9 @@ def simulated_annealing(temperatura_inicial, verboso=False):
             tabuleiro_atual = tabuleiro_seguinte
 
         if verboso is True:
-            print(f"[Iteração {iteracao}] {tabuleiro_atual} [Temperatura {temperatura}]")
+            print(f"[Iteração {iteracao}] {tabuleiro_atual} [Temperatura {np.round(temperatura, 3)}]")
+
+    if verboso is True:
+        print("A temperatura chegou a zero.")
+
+    return tabuleiro_atual
